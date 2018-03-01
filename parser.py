4eaabd7d66d76a8d91be907dd09ad8f50a049894
Paul Tyshevskyi <ptyshevs@student.unit.ua>
2018-03-01 20:49:09 +0200

    push again

--- parser.py
+++ parser.py
@@ -11,17 +11,33 @@ class Ride:
 		return "[{}, {}] -> [{}, {}] in time [{}, {}]".format(self.start_row, self.start_col, self.final_row,
 		self.final_col, self.start_time, self.final_time)
 
+class Car(object):
+	"""docstring for Car"""
+	def __init__(self, row, col, time_elapsed):
+		super(Car, self).__init__()
+		self.row = row
+		self.col = col
+		self.time_elapsed = time_elapsed
+		self.rides = []
+
+	def __str__(self):
+		return "[{}, {}] | current time {}".format(self.row, self.col, self.time_elapsed)
+
 
 class Metropolis:
-	def __init__(self, nrows, ncols, nvehicles, nrides, bonus, nsteps, rides: list):
+	def __init__(self, nrows, ncols, nvehicles, nrides, bonus, time, rides: list):
 		self.nrows = nrows
 		self.ncols = ncols
 		self.nvehicles = nvehicles
 		self.nrides = nrides
 		self.bonus = bonus
-		self.nsteps = nsteps
+		self.time = time
 		self.rides = rides
+		self.cars = [Car(0, 0, 0) for car in range(nvehicles)]
 
+	def __str__(self):
+		return "Grid [{}, {}] | Nvehicles: {} | Nrides: {} | Bonus: {} | Time: {}".format(
+		self.nrows, self.ncols, self.nvehicles, self.nrides, self.bonus, self.time)
 
 
 def parser(filename):
@@ -29,4 +45,6 @@ def parser(filename):
 		nrows, ncols, nvehicles, nrides, bonus, nsteps = map(int, f.readline().split())
 		rides = [f.readline().split() for n in range(nrides)]
 		metro = Metropolis(nrows, ncols, nvehicles, nrides, bonus, nsteps, [Ride(*ride) for ride in rides])
+		print(metro)
 		[print(ride) for ride in metro.rides]
+		[print(car) for car in metro.cars]
