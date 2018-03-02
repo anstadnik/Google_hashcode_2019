20e5e7f5c6ec729af08e918dd600978ec8430a25
Paul Tyshevskyi <ptyshevs@student.unit.ua>
2018-03-02 04:31:28 +0200

    44mln, baby. Come on, we can do better

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import sys
 from parser import *
 
-def output(metro, filename):
-	with open(filename, "w+") as f:
-		for car in metro.cars:
-			print(str(len(car.rides)) + " " + " ".join([str(ride.ride_number) for ride in car.rides]), file=f)
 
+def output(metro, filename):
+    with open(filename, "w+") as f:
+        for car in metro.cars:
+            print(str(len(car.rides)) + " " + " ".join([str(ride.ride_number) for ride in car.rides]), file=f)
 
 
 if __name__ == '__main__':
-	metro = parser(sys.argv[1])
-	very_good_algo(metro)
-	output(metro, sys.argv[2])
+    metro = parser(sys.argv[1])
+    very_good_algo(metro)
+    for car in metro.cars:
+        print(car)
+    output(metro, sys.argv[2])
--- parser.py
+++ parser.py
@@ -1,92 +1,117 @@
 class Ride:
-	def __init__(self, ridenumber, start_row, start_col, final_row, final_col, start_time, final_time):
-		self.ride_number = ridenumber
-		self.start_row = start_row
-		self.start_col = start_col
-		self.final_row = final_row
-		self.final_col = final_col
-		self.start_time = start_time
-		self.final_time = final_time
-		self.time_needed = manhetten_distance(start_row, start_col, final_row, final_col)
+    def __init__(self, ridenumber, start_row, start_col, final_row, final_col, start_time, final_time):
+        self.ride_number = ridenumber
+        self.start_row = start_row
+        self.start_col = start_col
+        self.final_row = final_row
+        self.final_col = final_col
+        self.start_time = start_time
+        self.final_time = final_time
+        self.time_needed = manhetten_distance(start_row, start_col, final_row, final_col)
 
-	def __str__(self):
-		return "[{}, {}] -> [{}, {}] in time [{}, {}]".format(self.start_row, self.start_col, self.final_row,
-		self.final_col, self.start_time, self.final_time)
+    def __str__(self):
+        return "[{}, {}] -> [{}, {}] in time [{}, {}]".format(self.start_row, self.start_col, self.final_row,
+                                                              self.final_col, self.start_time, self.final_time)
+
 
 class Car(object):
-	"""docstring for Car"""
-	def __init__(self, car_number, row, col, time_elapsed):
-		super(Car, self).__init__()
-		self.car_number = car_number
-		self.row = row
-		self.col = col
-		self.time_elapsed = time_elapsed
-		self.rides = []
+    """docstring for Car"""
+    def __init__(self, car_number, row, col, time_elapsed):
+        super(Car, self).__init__()
+        self.car_number = car_number
+        self.row = row
+        self.col = col
+        self.time_elapsed = time_elapsed
+        self.rides = []
 
-	def __str__(self):
-		return "Car [{}, {}] elapsed time {}\nRides:\n{}".format(self.row, self.col, self.time_elapsed,
-		[str(ride) for ride in self.rides])
-
-def manhetten_distance(x1, y1, x2, y2):
-	return abs(x2 - x1) + abs(y2 - y1)
-
-def time_needed(car: Car, ride: Ride):
-	#row это х, col это y
-	ret = manhetten_distance(car.row, car.col, ride.start_row, ride.start_col)
-	if ret + car.time_elapsed < ride.start_time:
-		ret = ride.start_time
-	ret += ride.time_needed
-	return ret
-
-def first_n(ncars, rides: list):
-	# пожалуйста
-	car = Car(0, 0, 0, 0)
-	try_this = sorted(rides, key=lambda ride: time_needed(car, ride))
-	return try_this[:ncars]
-
-def each_car(car, rides: list)->Ride:
-	return sorted(rides, key=lambda ride: time_needed(car, ride))[0]
-
-
-def very_good_algo(metro):
-	while metro.rides:
-		for i in range(metro.nvehicles):
-			if not metro.rides:
-				break
-			best_ride = each_car(metro.cars[i], metro.rides)
-			metro.cars[i].row = best_ride.final_row
-			metro.cars[i].col = best_ride.final_col
-			metro.cars[i].time_elapsed += best_ride.time_needed
-			metro.cars[i].rides.append(best_ride)
-			metro.rides.remove(best_ride)
-		print("rides left:")
-		[print(ride) for ride in metro.rides]
+    def __str__(self):
+        return "Car [{}, {}] elapsed time {}\nRides:\n{}".format(self.row, self.col, self.time_elapsed,
+                                                                 "\n".join([str(ride) for ride in self.rides]))
 
 
 class Metropolis:
-	def __init__(self, nrows, ncols, nvehicles, nrides, bonus, time, rides: list):
-		self.nrows = nrows
-		self.ncols = ncols
-		self.nvehicles = nvehicles
-		self.nrides = nrides
-		self.bonus = bonus
-		self.time = time
-		self.rides = rides
-		self.cars = [Car(i + 1, 0, 0, 0) for i in range(nvehicles)]
+    def __init__(self, nrows, ncols, nvehicles, nrides, bonus, time, rides: list):
+        self.nrows = nrows
+        self.ncols = ncols
+        self.nvehicles = nvehicles
+        self.nrides = nrides
+        self.bonus = bonus
+        self.time = time
+        self.rides = rides
+        self.cars = [Car(i, 0, 0, 0) for i in range(1, nvehicles + 1)]
 
-	def __str__(self):
-		return "Grid [{}, {}] | Nvehicles: {} | Nrides: {} | Bonus: {} | Time: {}".format(
-		self.nrows, self.ncols, self.nvehicles, self.nrides, self.bonus, self.time)
+    def __str__(self):
+        return "Grid [{}, {}] | Nvehicles: {} | Nrides: {} | Bonus: {} | Time: {}".format(
+            self.nrows, self.ncols, self.nvehicles, self.nrides, self.bonus, self.time)
+
+
+def manhetten_distance(x1, y1, x2, y2):
+    """
+    Calculate manhatten distance
+    :param x1: row
+    :param y1: col
+    :param x2: row
+    :param y2: col
+    :return:
+    """
+    return abs(x2 - x1) + abs(y2 - y1)
+
+
+def time_needed(car: Car, ride: Ride):
+    """row это х, col это y"""
+    ret = manhetten_distance(car.row, car.col, ride.start_row, ride.start_col)
+    if ret + car.time_elapsed < ride.start_time:
+        ret = ride.start_time
+    ret += ride.time_needed
+    return ret
+
+
+def first_n(ncars, rides: list):
+    # пожалуйста
+    car = Car(0, 0, 0, 0)
+    try_this = sorted(rides, key=lambda ride: time_needed(car, ride))
+    return try_this[:ncars]
+
+
+def custom_sort(time, car: Car, ride: Ride):
+    if ride.start_col == car.col and ride.start_row == car.row:
+        return int(time_needed(car, ride) * 0.9)
+    elif manhetten_distance(car.row, car.col, ride.start_row, ride.start_col) < 5 and \
+            ride.start_time <= car.time_elapsed + manhetten_distance(car.row, car.col, ride.start_row, ride.start_col):
+        return int(time_needed(car, ride) * 0.6)
+    elif car.time_elapsed + ride.time_needed > time:
+        return int(time_needed(car, ride) * 100)
+    else:
+        return time_needed(car, ride)
+
+
+def each_car(time, car, rides: list)->Ride:
+    return min(rides, key=lambda ride: custom_sort(time, car, ride))
+
+
+def very_good_algo(metro: Metropolis):
+    while metro.rides:
+        car = min(metro.cars, key=lambda car: len(car.rides))
+        best_ride = each_car(metro.time, car, metro.rides)
+        car.row = best_ride.final_row
+        car.col = best_ride.final_col
+        if car.time_elapsed < best_ride.start_time:
+            car.time_elapsed = best_ride.start_time + best_ride.time_needed
+        else:
+            car.time_elapsed += best_ride.time_needed
+        car.rides.append(best_ride)
+        metro.rides.remove(best_ride)
+        # print("rides left:")
+        # [print(ride) for ride in metro.rides]
 
 
 def parser(filename):
-	with open(filename) as f:
-		nrows, ncols, nvehicles, nrides, bonus, nsteps = map(int, f.readline().split())
-		rides = [f.readline().split() for n in range(nrides)]
-		print(rides)
-		metro = Metropolis(nrows, ncols, nvehicles, nrides, bonus, nsteps,
-			[Ride(i, *map(int,ride)) for i, ride in zip(range(nrides), rides)])
-		# print(metro)
-		# [print(ride) for ride in metro.rides]
-		# [print(car) for car in metro.cars]
-		return metro
+    with open(filename) as f:
+        nrows, ncols, nvehicles, nrides, bonus, nsteps = map(int, f.readline().split())
+        rides = [f.readline().split() for n in range(nrides)]
+        metro = Metropolis(nrows, ncols, nvehicles, nrides, bonus, nsteps,
+                           [Ride(i, *map(int,ride)) for i, ride in zip(range(nrides), rides)])
+        # print(metro)
+        # [print(ride) for ride in metro.rides]
+        # [print(car) for car in metro.cars]
+        return metro
